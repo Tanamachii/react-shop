034fe3606d078d2a68ae1af34ed526a5cbc72cf6
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2022-11-07 11:17:57 -0700

    Se agrega el product list al home view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import Header from "../components/Header";
+import ProductList from "../containers/ProductList";
 
 const Home = () => {
-  return;
+  return (
+    <>
+      <ProductList />;
+    </>
+  );
 };
 
 export default Home;
